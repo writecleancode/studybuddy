7d1ceb81c617d50b9b40ff24675ccc64b4729243
Rafal <rafek131@wp.pl>
2023-09-07 13:56:54 +0200

    Fixed styled-components imports

--- src/components/organisms/UsersList/UsersList.styles.js
+++ src/components/organisms/UsersList/UsersList.styles.js
@@ -1,10 +1,10 @@
-import { styled } from 'styled-components';
+import styled from 'styled-components';
 
 export const Wrapper = styled.div`
 	padding: 32px 64px;
 	border-radius: 25px;
-    width: 100%;
-    max-width: 500px;
+	width: 100%;
+	max-width: 500px;
 	background-color: #fff;
 	box-shadow: 0 5px 15px -10px rgba(0, 0, 0, 0.3);
 `;
