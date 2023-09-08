1bc8e0abae4306d044f12df188c9f2a022cce0a1
Rafal <rafek131@wp.pl>
2023-09-08 10:21:39 +0200

    Moved DeleteButton styles into separate '.styles' file

--- src/components/atoms/DeleteButton/DeleteButton.js
+++ src/components/atoms/DeleteButton/DeleteButton.js
@@ -1,17 +1,5 @@
 import { ReactComponent as DeleteIcon } from 'assets/icons/delete-icon.svg';
-import { styled } from 'styled-components';
-
-export const StyledButton = styled.button`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	margin-left: 16px;
-	border: none;
-	border-radius: 100px;
-	width: 24px;
-	height: 24px;
-	background-color: ${({ theme }) => theme.colors.grey};
-`;
+import { StyledButton } from './DeleteButton.styles';
 
 export const DeleteButton = () => {
 	return (
