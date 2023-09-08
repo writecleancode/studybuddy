dddd9c0cd18a9708f68adfc2ba3ff7dc12a5350e
Rafal <rafek131@wp.pl>
2023-09-08 13:56:32 +0200

    Added fake API to simulate the use of 'useEffect'

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,15 +1,33 @@
 import { users as usersData } from 'data/users';
 import { UsersListItem } from 'components/molecules/UsersListItem/UsersListItem';
 import { StyledList, Wrapper } from './UsersList.styles';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const mockAPI = success => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if (usersData) {
+				resolve([...usersData]);
+			} else {
+				reject({ message: 'Error' });
+			}
+		}, 2000);
+	});
+};
 
 export const UsersList = () => {
-	const [users, setUsers] = useState(usersData);
+	const [users, setUsers] = useState([]);
 
 	const deleteUser = name => {
 		const filteredUsers = users.filter(user => user.name !== name);
 		setUsers(filteredUsers);
 	};
+
+	useEffect(() => {
+		mockAPI()
+			.then(data => setUsers(data))
+			.catch(err => console.log(err));
+	}, []);
 
 	return (
 		<Wrapper>
