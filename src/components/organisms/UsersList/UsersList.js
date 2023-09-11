04e90e35195b526ae7425b02785a6840659e4d09
Rafal <rafek131@wp.pl>
2023-09-11 10:44:05 +0200

    Removed fake API to simulate the use of 'useEffect'

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,40 +1,17 @@
 import { users as usersData } from 'data/users';
 import { UsersListItem } from 'components/molecules/UsersListItem/UsersListItem';
 import { StyledList, StyledTitle, Wrapper } from './UsersList.styles';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { FormField } from 'components/molecules/UsersListItem/FormField/FormField';
 import { Button } from 'components/atoms/Button/Button';
 
-const mockAPI = success => {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			if (usersData) {
-				resolve([...usersData]);
-			} else {
-				reject({ message: 'Error' });
-			}
-		}, 2000);
-	});
-};
-
 export const UsersList = () => {
-	const [users, setUsers] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
+	const [users, setUsers] = useState(usersData);
 
 	const deleteUser = name => {
 		const filteredUsers = users.filter(user => user.name !== name);
 		setUsers(filteredUsers);
 	};
-
-	useEffect(() => {
-		setIsLoading(true);
-		mockAPI()
-			.then(data => {
-				setUsers(data);
-				setIsLoading(false);
-			})
-			.catch(err => console.log(err));
-	}, []);
 
 	return (
 		<>
@@ -47,7 +24,7 @@ export const UsersList = () => {
 			</Wrapper>
 			<Wrapper>
 				<StyledList>
-					<StyledTitle>{isLoading ? 'Loading...' : "Student's list"}</StyledTitle>
+					<StyledTitle>Student's list</StyledTitle>
 					{users.map(userData => {
 						return (
 							<UsersListItem key={userData.name} userData={userData} deleteUser={deleteUser} />
