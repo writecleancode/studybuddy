9d09efa004291d4e39ba135a870386657e026e84
Rafal <rafek131@wp.pl>
2023-09-19 15:28:43 +0200

    Extended 'MainTemplate' to make place for SearchBar and NewsSection

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -1,11 +1,26 @@
-import { Wrapper } from './MainTemplate.styles';
 import { Navigation } from 'components/organisms/Navigation/Navigation';
+import { Wrapper } from './MainTemplate.styles';
+import styled from 'styled-components';
+
+export const SaerchBar = styled.div`
+	grid-row: 1 / 2;
+	grid-column: 2 / 3;
+	border-bottom: 1px solid ${({ theme }) => theme.colors.darkPurple};
+`;
+
+export const News = styled.div`
+	grid-row: 1 / 3;
+	grid-column: 3 / 4;
+	border-left: 1px solid ${({ theme }) => theme.colors.darkPurple};
+`;
 
 export const MainTemplate = ({ children }) => {
 	return (
 		<Wrapper>
 			<Navigation />
+			<SaerchBar />
 			{children}
+			<News />
 		</Wrapper>
 	);
 };
--- src/components/templates/MainTemplate/MainTemplate.styles.js
+++ src/components/templates/MainTemplate/MainTemplate.styles.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
 	display: grid;
-	grid-template-columns: 150px 1fr;
-    min-height: 100vh;
+	grid-template-rows: 92px 1fr;
+	grid-template-columns: 150px 1fr 0.75fr;
+	min-height: 100vh;
 	background-color: ${({ theme }) => theme.colors.lightGrey};
 `;
