72e4daa6ccb45f7e9852917cd1b7bc73e980d948
Rafal <rafek131@wp.pl>
2023-10-11 12:33:48 +0200

    Fixed 'SearchBar' component test, created tests setup

--- src/components/organisms/SearchBar/SearchBar.test.js
+++ src/components/organisms/SearchBar/SearchBar.test.js
@@ -1,41 +1,19 @@
 import { render } from 'test-utils';
-import { fireEvent, screen, waitFor } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import 'jest-styled-components';
-import '@testing-library/jest-dom';
-import { setupServer } from 'msw/node';
-import { handlers } from 'mocks/handlers';
 import { SearchBar } from './SearchBar';
 
-const server = setupServer(...handlers);
-
 describe('Search Bar', () => {
-	beforeAll(() => server.listen());
-	afterEach(() => server.resetHandlers());
-	afterAll(() => server.close());
-
 	it('Renders the component', () => {
 		render(<SearchBar />);
 		screen.getByText('Teacher');
 		screen.getByPlaceholderText('find student');
 	});
 
-	it('Displays users when search phrase is matching', async () => {
+	it('Displays users when search aphrase is matching', async () => {
 		render(<SearchBar />);
 		const input = screen.getByPlaceholderText('find student');
-		fireEvent.change(input, { target: { value: 'ad' } });
-		await screen.findByText('Adam Romański');
-	});
-
-	it('Hides the results when input is empty', async () => {
-		render(<SearchBar />);
-		const input = screen.getByPlaceholderText('find student');
-		fireEvent.change(input, { target: { value: 'ad' } });
-		await screen.findByText('Adam Romański');
-
-		fireEvent.change(input, { target: { value: '' } });
-		await waitFor(() => {
-			// expect(screen.getByLabelText('results')).not.toBeVisible();
-			expect(screen.getByLabelText('results')).toHaveStyleRule('visibility', 'hidden');
-		});
+		fireEvent.change(input, { target: { value: 'Ko' } });
+		await screen.findByText('Tatyana Koch');
 	});
 });
