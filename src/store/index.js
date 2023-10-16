0158c30cc95fb2e9c9ae76f6d90dbca903a3ef55
Rafal <rafek131@wp.pl>
2023-10-16 14:00:32 +0200

    Changed add new notes handler to 'Redux-Toolkit'

--- src/store/index.js
+++ src/store/index.js
@@ -1,42 +1,26 @@
-import { createStore } from 'redux';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 import { v4 as uuid } from 'uuid';
 
-export const addNote = payload => {
-	return {
-		type: 'notes/add',
-		payload: {
-			id: uuid(),
-			...payload,
+const initialNotesState = [];
+
+const notesSlice = createSlice({
+	name: 'notes',
+	initialState: initialNotesState,
+	reducers: {
+		addNote(state, action) {
+			state.push({
+				id: uuid(),
+				...action.payload,
+			});
 		},
-	};
-};
+		removeNote(state, action) {},
+	},
+});
 
-export const removeNote = payload => {
-	return {
-		type: 'notes/remove',
-		payload: payload,
-	};
-};
+export const { addNote, removeNote } = notesSlice.actions;
 
-const initialState = {
-	notes: [],
-};
-
-const notesReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case 'notes/add':
-			return {
-				...state,
-				notes: [...state.notes, action.payload],
-			};
-		case 'notes/remove':
-			return {
-				...state,
-				notes: state.notes.filter(note => note.id !== action.payload.id),
-			};
-		default:
-			return state;
-	}
-};
-
-export const store = createStore(notesReducer);
+export const store = configureStore({
+	reducer: {
+		notes: notesSlice.reducer,
+	},
+});
